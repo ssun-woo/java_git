25ee1750135a397fcc3f3690d5b2211703396f4b
sw <sw@naver.com>
2023-04-27 11:04:09 +0900

    java_git 프로젝트

---
+++ src/main/MainClass.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int num;
+		while (true) {
+			System.out.println("1. 로그인 기능");
+			System.out.println("2. 회원 기능");
+			System.out.println("3. 수정 기능");
+			System.out.println("4. 게시판 기능");
+			num = input.nextInt();
+
+			switch (num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			}
+		}
+
+	}
+
+}
